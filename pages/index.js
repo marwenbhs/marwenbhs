96e8bbfc7581049170dac7bcc5fd787c7c835508
marwenbhs <97807028+marwenbhs@users.noreply.github.com>
2022-03-03 11:40:10 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="waw" />
         <p className="description">
-          THIS IS  <code img src="/public/marwen.jpg"><img src="/public/marwen.jpg"/>MARWEN</code>
+          THIS IS <img src="/public/marwen.jpg"/> <code >MARWEN</code>
         </p>
       </main>
 
