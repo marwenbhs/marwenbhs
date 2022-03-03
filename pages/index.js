659d023c1560c878d4ba031e89b2901c32d18ec7
marwenbhs <97807028+marwenbhs@users.noreply.github.com>
2022-03-03 10:44:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="41.226.11.54" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          THIS IS  <code>MARWEN</code>
         </p>
       </main>
 
